326b6c9a23d344fac9421cb9e8f4b0abcb733e8d
ditzzcode <radityahuuu@gmail.com>
2024-12-14 10:20:02 +0800

    belajar menggunakan useState

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import MyButton from './components/MyButton.jsx'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
+import { useState } from 'react'
 
 function App() {
   const headerText = "ini halaman header / utama";
@@ -17,11 +18,22 @@ function App() {
       <h1>halaman footer</h1>
     )
   }
+
+  const clicked = () => {
+    return alert("button ini di klick")
+  }
+
+  const [getValue , setValue] = useState(0)
+
+
   return (
     <>
   <Header headerText={headerText}/>
-  <MyButton/>
+  <MyButton click={clicked}/>
   <Navbar/>
+  <div>{getValue}</div>
+  <button onClick={() => setValue(getValue + 1)}>plus</button>
+  <button onClick={() => setValue(getValue - 1)}>minus</button>
   <Footer footerText={footerText} footerHi={footerHi}/>
     </>
   )
--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,7 +1,21 @@
- const MyButton = () => {
+import { useState } from "react";
+
+const MyButton = () => {
+
+const [getValue , setValue] = useState(0)
+
+const nambahData = () => {
+if (getValue < 20 ) {
+  setValue(getValue + 1)
+} else {
+  return alert("cukup bos")
+}
+}
+
   return (
     <div>
-      <button>Click me bro!</button>
+      <div>{getValue}</div>
+      <button onClick={() => nambahData()}>Click me bro!</button>
     </div>
   )
 };
